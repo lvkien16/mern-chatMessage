d3e9b0ad7aebf51b5126ec594fd85a8f38e2862a
kienprovip <lovankien@gmail.com>
2024-04-22 20:23:41 +0700

    create pages and roues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Friends from "./pages/Friends";
+import Messages from "./pages/Messages";
+import Dashboard from "./pages/Dashboard";
+import Notifications from "./pages/Notifications";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
-  return <div className="text-red-700">App</div>;
+  return (
+    <BrowserRouter className="text-red-700">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
